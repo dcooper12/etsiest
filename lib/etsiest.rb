d8add31613764b2d05dba0620d792ab5370e69b5
Darryl Cooper <darryl.cooper12@gmail.com>
2015-10-26 01:54:47 -0400

    images not lined up

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -3,6 +3,20 @@ require "sinatra/base"
 require "pry"
 require "etsy"
 
+
+Etsy.api_key = ENV['ETSY_KEY']
+
 module Etsiest
-  # Your code goes here...
+  class App < Sinatra::Base
+  	set :logging, true
+
+  	get "/" do
+  		query = "whiskey"
+		  response = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => query)
+		  items = response.result
+		  erb :index, locals: { items: items }
+	  end
+
+  	run! if app_file == $0
+  end
 end
